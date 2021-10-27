caf122844aa731f821f295d7c3248540f5d5c04b
Mariia Shreider <shr.masha2001@gmail.com>
2021-10-27 20:23:43 +0300

    index of coincidence function

--- cp2/shreider_zhembrovska_fb93_cp2/main.py
+++ cp2/shreider_zhembrovska_fb93_cp2/main.py
@@ -3,10 +3,9 @@ from collections import Counter
 with open('text.txt', 'r', encoding = 'utf-8') as f:
     plaintext = f.read()
 
-alph = ['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
+alph = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 keys = ['ай','кот','мать','жесть','разложение','превращение','трагикомедия','недоразумение','нравственность',
         'амариллисцветок','мягенькаяподушка','пельменискетчупом','неспатьаделатьлабу','киберполицияукраины','женоненавистничесвто']
-
 
 def encryption(text,key):
     ciphertext = []
@@ -15,4 +14,10 @@ def encryption(text,key):
         ciphertext.append(alph[y])
     return ''.join(i for i in ciphertext)
 
-
+def coindex(text):
+    nt = Counter(text)
+    index = 0
+    for i in nt:
+        index += nt[i]*(nt[i]-1)
+    index /= (len(text)*(len(text)-1))
+    return index
