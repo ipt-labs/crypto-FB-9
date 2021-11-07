7f9076ff83c7b81a48d43aa3f881427f9600dbd0
Qub13 <danilbe168@gmail.com>
2021-11-07 22:42:33 +0200

    need fix write to file

--- cp1/Belotskyi_fb94_Reznicnenko_fb94/cmd/main.go
+++ cp1/Belotskyi_fb94_Reznicnenko_fb94/cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -63,37 +63,7 @@ func lettersCount(text string) map[string]int{
 
 	lettersCount := map[string]int{
 
-		"а": 0,
-		"б": 0,
-		"в": 0,
-		"г": 0,
-		"д": 0,
-		"е": 0,
-		"ж": 0,
-		"з": 0,
-		"и": 0,
-		"й": 0,
-		"к": 0,
-		"л": 0,
-		"м": 0,
-		"н": 0,
-		"о": 0,
-		"п": 0,
-		"р": 0,
-		"с": 0,
-		"т": 0,
-		"у": 0,
-		"ф": 0,
-		"х": 0,
-		"ц": 0,
-		"ч": 0,
-		"ш": 0,
-		"щ": 0,
-		"ы": 0,
-		"ь": 0,
-		"э": 0,
-		"ю": 0,
-		"я": 0,
+		"а": 0, "б": 0, "в": 0, "г": 0, "д": 0, "е": 0, "ж": 0, "з": 0, "и": 0, "й": 0, "к": 0, "л": 0, "м": 0, "н": 0, "о": 0, "п": 0, "р": 0, "с": 0, "т": 0, "у": 0, "ф": 0, "х": 0, "ц": 0, "ч": 0, "ш": 0, "щ": 0, "ы": 0, "ь": 0, "э": 0, "ю": 0, "я": 0,
 	}
 
 	alphabet := []string{"а", "б","в", "г", "д", "е", "ж","з", "a", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ы", "ь", "э", "ю", "я"}
@@ -124,38 +94,7 @@ type kv struct{
 func frequency(text string) map[string]float64 {
 
 	lettersFrequency := map[string]float64{
-
-		"а": 0,
-		"б": 0,
-		"в": 0,
-		"г": 0,
-		"д": 0,
-		"е": 0,
-		"ж": 0,
-		"з": 0,
-		"и": 0,
-		"й": 0,
-		"к": 0,
-		"л": 0,
-		"м": 0,
-		"н": 0,
-		"о": 0,
-		"п": 0,
-		"р": 0,
-		"с": 0,
-		"т": 0,
-		"у": 0,
-		"ф": 0,
-		"х": 0,
-		"ц": 0,
-		"ч": 0,
-		"ш": 0,
-		"щ": 0,
-		"ы": 0,
-		"ь": 0,
-		"э": 0,
-		"ю": 0,
-		"я": 0,
+		"а": 0, "б": 0, "в": 0, "г": 0, "д": 0, "е": 0, "ж": 0, "з": 0, "и": 0, "й": 0, "к": 0, "л": 0, "м": 0, "н": 0, "о": 0, "п": 0, "р": 0, "с": 0, "т": 0, "у": 0, "ф": 0, "х": 0, "ц": 0, "ч": 0, "ш": 0, "щ": 0, "ы": 0, "ь": 0, "э": 0, "ю": 0, "я": 0,
 	}
 
 	lettersCount(text)
@@ -189,46 +128,91 @@ func frequency(text string) map[string]float64 {
 
 }
 
-func createBgrammsMatrix(text string) []string {
+func createCountBgrammsMatrix(text string) []string {
 
 	tempArray := strings.Split(text, "")
 
 	if len(tempArray) % 2 == 0 {
 		tempArray = append(tempArray, "ю")
+
 	}
 
-	f, err := os.OpenFile("../docs/bgrams.txt", os.O_RDWR|os.O_CREATE, 0666)
+	var crossedBigrmas []string
+	var unCrossedBigrmas []string
+
+	for i := 0; i < len(tempArray) - 1; i++{
+		crossedBigrmas = append(crossedBigrmas, strings.ToLower(tempArray[i]) + strings.ToLower(tempArray[i+1]))
+	}
+
+	for i := 0; i < len(tempArray) - 2; i++{
+		unCrossedBigrmas = append(unCrossedBigrmas, strings.ToLower(tempArray[i]) + strings.ToLower(tempArray[i+2]))
+	}
+
+
+	crossBgrammsCount := map[string]int{}
+	UncrossBgrammsCount := map[string]int{}
+
+	for i:=0; i < len(crossedBigrmas); i++{
+		if _, ok := crossBgrammsCount[crossedBigrmas[i]]; ok{
+			crossBgrammsCount[crossedBigrmas[i]]++
+		} else {
+			crossBgrammsCount[crossedBigrmas[i]] = 1
+		}
+	}
+
+	for i:=0; i < len(unCrossedBigrmas); i++{
+		if _, ok := UncrossBgrammsCount[unCrossedBigrmas[i]]; ok{
+			UncrossBgrammsCount[unCrossedBigrmas[i]]++
+		} else {
+			UncrossBgrammsCount[unCrossedBigrmas[i]] = 1
+		}
+	}
+
+	fmt.Println(crossBgrammsCount, "\n")
+	fmt.Println(UncrossBgrammsCount)
+
+	CrossF, err := os.OpenFile("../docs/crossedBgrams.txt", os.O_RDWR|os.O_CREATE, 0666)
 	check(err)
 
-	truncateErr := f.Truncate(0)
+
+
+	UncrossF, err := os.OpenFile("../docs/UnCrossedBgrams.txt", os.O_RDWR|os.O_CREATE, 0666)
+	check(err)
+
+
+	truncateErr := CrossF.Truncate(0)
+	check(truncateErr)
+
+	truncateErr = UncrossF.Truncate(0)
 	check(truncateErr)
 
 
-	dataWriter := bufio.NewWriter(f)
 
-	var lettersArray []string
-	for i := 0; i <= len(tempArray) - 2; i++{
-		lettersArray = append(lettersArray, strings.ToLower(tempArray[i]) + strings.ToLower(tempArray[i+1]))
+	for key, value := range crossBgrammsCount{
+		_, err := CrossF.Write([]byte(string(key) + ": " + string(rune(value)) + "\n"))
+		check(err)
 	}
 
-	for _, data := range lettersArray{
-		_,_ = dataWriter.WriteString(data + " ")
-	}
 
-	err2 := dataWriter.Flush()
-	check(err2)
+	for key, value := range UncrossBgrammsCount{
+			_, err := UncrossF.Write([]byte(string(key) + ": " + string(rune(value)) + "\n"))
+			check(err)
+		}
 
-	err3 := f.Close()
-	check(err3)
+	//defer func(CrossF *os.File) {
+	//	err := CrossF.Close()
+	//	check(err)
+	//}(CrossF)
+	//defer func(CrossF *os.File) {
+	//	err := CrossF.Close()
+	//	check(err)
+	//}(CrossF)
 
-	return lettersArray
+
+	return crossedBigrmas
 }
 
-
-
 func main(){
-
-
 
 	//count := utf8.RuneCountInString(withOutSpaces)
 	//fmt.Println(count)
@@ -240,6 +224,6 @@ func main(){
 
 	_, withOutSpaces :=  replaceLettersSpaces("../docs/text.txt")
 
-	createBgrammsMatrix(withOutSpaces)
+	createCountBgrammsMatrix(withOutSpaces)
 
 }
