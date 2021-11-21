385a2b219ae811be3a10fe21828ce09d93e69610
Danil <danilbe168@gmail.com>
2021-11-21 19:16:03 +0200

    close to ready entropy bigrams not ready

--- cp1/Belotskyi_fb94_Reznicnenko_fb94/cmd/main.go
+++ cp1/Belotskyi_fb94_Reznicnenko_fb94/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
 	"regexp"
 	"strconv"
 
@@ -13,26 +14,32 @@ import (
 	"unicode/utf8"
 )
 
-var alphabet = []string{"а", "б","в", "г", "д", "е", "ж","з", "a", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ы", "ь", "э", "ю", "я"}
+var alphabet = []string{"а", "б", "в", "г", "д", "е", "ж", "з", "a", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ы", "ь", "э", "ю", "я"}
 
 var textPath string = "../docs/text.txt"
+
 //func for checking errors
-func check(err error){
-	if err != nil{
+func check(err error) {
+	if err != nil {
 		log.Fatal(err)
 
 	}
 }
 
 //func that replace letter in text, deleting spaces and returns array of text letters
-func replaceLettersSpaces(path string) (string, string){
+func replaceLettersSpaces(path string) (string, string) {
+
+
 
 	fileBytes, err := ioutil.ReadFile(path)
 	check(err)
 	sliceData := strings.Split(string(fileBytes), "\n")
 
-	var re = regexp.MustCompile(`ё`)
-	tempText := re.ReplaceAllString(sliceData[0], `е`)
+	newLine := regexp.MustCompile("\n")
+	withoutNewLine := newLine.ReplaceAllString(sliceData[0], ``)
+
+	re := regexp.MustCompile(`ё`)
+	tempText := re.ReplaceAllString(withoutNewLine, `е`)
 
 	re = regexp.MustCompile(`ъ`)
 	withSpaces := re.ReplaceAllString(string(tempText), `ь`)
@@ -57,7 +64,7 @@ func replaceLettersSpaces(path string) (string, string){
 
 	_, err3 := f2.WriteString(withoutSpaces)
 	check(err3)
-	//
+
 	//fmt.Println(withSpaces, "\n")
 	//fmt.Println(withoutSpaces, "\n")
 
@@ -66,7 +73,7 @@ func replaceLettersSpaces(path string) (string, string){
 }
 
 //func that counts each letter in text
-func lettersCount(text string) map[string]int{
+func lettersCount(text string) map[string]int {
 
 	lettersCount := map[string]int{
 		"а": 0, "б": 0, "в": 0, "г": 0, "д": 0, "е": 0, "ж": 0, "з": 0, "и": 0, "й": 0, "к": 0, "л": 0, "м": 0, "н": 0, "о": 0, "п": 0, "р": 0, "с": 0, "т": 0, "у": 0, "ф": 0, "х": 0, "ц": 0, "ч": 0, "ш": 0, "щ": 0, "ы": 0, "ь": 0, "э": 0, "ю": 0, "я": 0,
@@ -74,9 +81,9 @@ func lettersCount(text string) map[string]int{
 
 	lettersArray := strings.Split(text, "")
 
-	for i, _ := range lettersArray{
+	for i, _ := range lettersArray {
 		for j, _ := range alphabet {
-			if alphabet[j] == strings.ToLower(lettersArray[i])  {
+			if alphabet[j] == strings.ToLower(lettersArray[i]) {
 				lettersCount[strings.ToLower(lettersArray[i])]++
 				continue
 			}
@@ -87,8 +94,8 @@ func lettersCount(text string) map[string]int{
 }
 
 //structure for sorting array
-type kv struct{
-	Key string
+type kv struct {
+	Key   string
 	Value float64
 }
 
@@ -107,12 +114,12 @@ func letterFrequency(text string) {
 	count := utf8.RuneCountInString(text)
 	fmt.Println(count)
 
-	for i, _ := range lettersFrequency{
+	for i, _ := range lettersFrequency {
 		lettersFrequency[i] = float64(letters[i]) / float64(count)
 	}
 
 	var sortArr []kv
-	for k, v := range lettersFrequency{
+	for k, v := range lettersFrequency {
 		sortArr = append(sortArr, kv{k, v})
 	}
 
@@ -120,7 +127,7 @@ func letterFrequency(text string) {
 		return sortArr[i].Value > sortArr[j].Value
 	})
 
-	file, err := os.Create("../docs/lettersFrequency")
+	file, err := os.Create("../docs/lettersFrequency.txt")
 	check(err)
 	defer func(file *os.File) {
 		err := file.Close()
@@ -128,9 +135,14 @@ func letterFrequency(text string) {
 	}(file)
 
 	var frequencyToFile string
+	var entropy float64
 
-	for i:=0; i<len(sortArr);i++{
+
+	for i := 0; i < len(sortArr); i++ {
 		tempKey := sortArr[i].Key
+
+		entropy += -sortArr[i].Value * math.Log2(sortArr[i].Value)
+
 		tempVal := strconv.FormatFloat(sortArr[i].Value, 'E', -1, 64)
 
 		frequencyToFile = tempKey + ": " + tempVal + "\n"
@@ -139,42 +151,45 @@ func letterFrequency(text string) {
 		check(err)
 
 	}
+
+	_, errEnt := file.WriteString("\nLetters Entropy: " +strconv.FormatFloat(entropy, 'f', 6, 64))
+	check(errEnt)
+
 }
 
 func createCountBgrammsMatrix(text string) []string {
 
 	tempArray := strings.Split(text, "")
 
-	if len(tempArray) % 2 == 0 {
-		tempArray = append(tempArray, "ю")
+	if len(tempArray)%2 == 0 {
 
+		tempArray = append(tempArray, "ю")
 	}
 
 	var crossedBigrmas []string
 	var unCrossedBigrmas []string
 
-	for i := 0; i < len(tempArray) - 1; i++{
-		crossedBigrmas = append(crossedBigrmas, strings.ToLower(tempArray[i]) + strings.ToLower(tempArray[i+1]))
+	for i := 0; i < len(tempArray)-1; i++ {
+		crossedBigrmas = append(crossedBigrmas, strings.ToLower(tempArray[i])+strings.ToLower(tempArray[i+1]))
 	}
 
-	for i := 0; i < len(tempArray) - 2; i++{
-		unCrossedBigrmas = append(unCrossedBigrmas, strings.ToLower(tempArray[i]) + strings.ToLower(tempArray[i+2]))
+	for i := 0; i < len(tempArray)-2; i++ {
+		unCrossedBigrmas = append(unCrossedBigrmas, strings.ToLower(tempArray[i])+strings.ToLower(tempArray[i+2]))
 	}
-
 
 	crossBgrammsCount := map[string]int{}
 	UncrossBgrammsCount := map[string]int{}
 
-	for i:=0; i < len(crossedBigrmas); i++{
-		if _, ok := crossBgrammsCount[crossedBigrmas[i]]; ok{
+	for i := 0; i < len(crossedBigrmas); i++ {
+		if _, ok := crossBgrammsCount[crossedBigrmas[i]]; ok {
 			crossBgrammsCount[crossedBigrmas[i]]++
 		} else {
 			crossBgrammsCount[crossedBigrmas[i]] = 1
 		}
 	}
 
-	for i:=0; i < len(unCrossedBigrmas); i++{
-		if _, ok := UncrossBgrammsCount[unCrossedBigrmas[i]]; ok{
+	for i := 0; i < len(unCrossedBigrmas); i++ {
+		if _, ok := UncrossBgrammsCount[unCrossedBigrmas[i]]; ok {
 			UncrossBgrammsCount[unCrossedBigrmas[i]]++
 		} else {
 			UncrossBgrammsCount[unCrossedBigrmas[i]] = 1
@@ -187,33 +202,66 @@ func createCountBgrammsMatrix(text string) []string {
 	UncrossF, err := os.OpenFile("../docs/UnCrossedBgrams.txt", os.O_RDWR|os.O_CREATE, 0666)
 	check(err)
 
-
 	truncateErr := CrossF.Truncate(0)
 	check(truncateErr)
 
 	truncateErr = UncrossF.Truncate(0)
 	check(truncateErr)
 
+	for key, value := range crossBgrammsCount {
 
+		strVal := strconv.Itoa(value)
 
-	for key, value := range crossBgrammsCount{
-		_, err := CrossF.Write([]byte(string(key) + ": " + string(rune(value)) + "\n"))
+		_, err := CrossF.WriteString(key + ": " + strVal + "\n")
+		check(err)
+
+	}
+
+	for key, value := range UncrossBgrammsCount {
+
+		strVal := strconv.Itoa(value)
+
+		_, err := UncrossF.WriteString(key + ": " + strVal + "\n")
 		check(err)
 	}
 
+	CrossFreqF, err := os.OpenFile("../docs/CrossedBgramsFreq.txt", os.O_RDWR|os.O_CREATE, 0666)
+	check(err)
+	defer CrossFreqF.Close()
 
-	for key, value := range UncrossBgrammsCount{
-			_, err := UncrossF.Write([]byte(string(key) + ": " + string(rune(value)) + "\n"))
-			check(err)
-		}
+	UncrossFreqF, err := os.OpenFile("../docs/UnCrossedBgramsFreq.txt", os.O_RDWR|os.O_CREATE, 0666)
+	check(err)
+	defer UncrossFreqF.Close()
 
+	lenText := len(text)
+	floatValText := float64(lenText)
+
+	for key, value := range crossBgrammsCount {
+
+		var floatVal float64 = float64(value) / (floatValText - 1)
+
+		strVal := strconv.FormatFloat(floatVal, 'f', 6, 64)
+
+		_, err := CrossFreqF.WriteString(key + ": " + strVal + "\n")
+		check(err)
+	}
+
+	for key, value := range UncrossBgrammsCount {
+
+		var floatVal float64 = float64(value) / 2
+
+		strVal := strconv.FormatFloat(floatVal, 'f', 6, 64)
+
+		_, err := UncrossFreqF.WriteString(key + ": " + strVal + "\n")
+		check(err)
+	}
 
 	return crossedBigrmas
 }
 
-func main(){
+func main() {
 
-	_, withOutSpace :=  replaceLettersSpaces(textPath)
+	_, withOutSpace := replaceLettersSpaces(textPath)
 
 	createCountBgrammsMatrix(withOutSpace)
 
